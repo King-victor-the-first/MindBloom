d7ed5208d710bc824ca40f5aea9c64ac1889835b
Victor Ehebhoria <victorehebhoria@gmail.com>
2025-10-07 10:57:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8972653883-d4ab8",
+  "appId": "1:1072461112497:web:037288a752f8b374d9ee90",
+  "apiKey": "AIzaSyCXXNShHn547cV_82b-Hbk-sF82Oz4H6_k",
+  "authDomain": "studio-8972653883-d4ab8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1072461112497"
+};
